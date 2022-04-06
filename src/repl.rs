7dc7cfafb41a6d31cc08f5f49c9e9671e97b11e5
marcaube <marcaube@users.noreply.github.com>
2022-04-06 12:54:07 -0400

    Add a basic REPL

---
+++ src/repl.rs
@@ -0,0 +1,29 @@
+use std::io::{self, Stdin, Stdout, Write};
+
+use crate::{lexer::Lexer, token::TokenType};
+
+pub fn start() {
+    let stdin: Stdin = io::stdin();
+    let mut stdout: Stdout = io::stdout();
+    let mut input = String::new();
+
+    print!("This is the Rusty Monkey REPL, feel free to type in commands!\n");
+
+    loop {
+        print!(">>> ");
+        stdout.flush().expect("Failed to flush output to stdout!");
+        stdin
+            .read_line(&mut input)
+            .expect("Failed to read line from stdin!");
+
+        let mut lexer = Lexer::new(input.trim());
+        let mut tok = lexer.next_token();
+
+        while tok.token_type != TokenType::Eof {
+            print!("{:?}\n", tok);
+            tok = lexer.next_token();
+        }
+
+        input.clear();
+    }
+}
