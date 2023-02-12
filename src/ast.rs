e227e99f91a4dc7b241fcdab0229ef57d36970be
marcaube <marcaube@users.noreply.github.com>
2023-02-11 19:42:51 -0500

    Implement Display for Statement

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 /// This is the root node of our AST
 pub struct Program {
     pub statements: Vec<Statement>,
@@ -7,4 +9,13 @@ pub struct Program {
 pub enum Statement {
     Let(String),
     Return, // TODO: add expression
+}
+
+impl fmt::Display for Statement {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Statement::Let(ident) => write!(f, "let {};", ident),
+            Statement::Return => write!(f, "return;"),
+        }
+    }
 }
